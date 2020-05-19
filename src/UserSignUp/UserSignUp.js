d7e7c374309d54d2f9508308f8d1f9670703f639
Matheus Henrique Pereira Santos <lamatheus.santos@MacBook-Pro-de-Matheus.local>
2020-05-19 18:04:04 -0300

    [Laura|Matheus] Applies style to User Sign Up form

--- src/UserSignUp/UserSignUp.js
+++ src/UserSignUp/UserSignUp.js
@@ -1,21 +1,43 @@
 import React from 'react'
 import {User} from '../api'
 
+
+
 const handleSubmit = async (event) => {
     event.preventDefault()
     const userInfo = {
         name: event.target.elements.name.value,
         email: event.target.elements.email.value
     }
+    event.target.reset()
     await User.saveNew(userInfo)
+
 }
 
 const UserSignUp = ({ onSubmit = handleSubmit }) => {
     return(
-        <form onSubmit={onSubmit}>
-            <input type="text" data-testid="name" id="name" />
-            <input type="text" data-testid="email" id="email" />
-            <input type="submit" data-testid="submit"/>
+        <form onSubmit={onSubmit} style={{"margin": "0 auto", "width": "40vw"}}>
+            <h1 className="h3 mb-3 font-weight-normal">Cultivatr</h1>
+            <h2>🌱🌿🍀</h2>
+            <div className="form-group">
+                <input 
+                    type="text" 
+                    id="name" 
+                    data-testid="name" 
+                    placeholder="Nome" 
+                    className="form-control" />
+                <input 
+                    type="text" 
+                    id="email" 
+                    data-testid="email" 
+                    placeholder="E-mail"
+                    className="form-control" />
+            </div>    
+            <input 
+                type="submit" 
+                data-testid="submit" 
+                label="Salvar"
+                className="btn btn-primary btn-lg btn-block"/>
         </form>
     )
 }
--- src/UserSignUp/UserSignUp.spec.js
+++ src/UserSignUp/UserSignUp.spec.js
@@ -3,6 +3,7 @@ import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import axiosMock from 'axios'
 import UserSignUp from './UserSignUp.js'
+import baseURL from '../base'
 
 jest.mock('axios')
 
@@ -23,13 +24,12 @@ test('submits user name and email', async () => {
         name: 'Agatha Silva',
         email: 'agatha@example.com',
     }
-    const mockedURL = 'https://cultivatr-api.herokuapp.com/user' 
+    const mockedURL = `${baseURL}/user` 
 
     const form = render(<UserSignUp />)
     const name = form.getByTestId('name')
     const email = form.getByTestId('email')
     const submit = form.getByTestId('submit')
-    
     fireEvent.change(name, { target: { value: mockedUserInfo.name } })
     fireEvent.change(email, { target: { value: mockedUserInfo.email } })
     fireEvent.click(submit)
