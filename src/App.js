bcb7e2e54b1898e80d225d75c11dc4e94720bb86
Matheus Henrique Pereira Santos <lamatheus.santos@MacBook-Pro-de-Matheus.local>
2020-05-26 10:09:19 -0300

    [Laura|LF|Matheus] Adds google login button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import HelloApi from './api';
 import UserSignUp from './UserSignUp/UserSignUp'
+import GoogleLogin from 'react-google-login'
+
+const responseGoogle = (response) => {
+  console.log(response);
+}
 
 async function getHello(name) {
     const hello = await HelloApi.get(name)
@@ -11,6 +16,13 @@ function App() {
   return (
     <div className="text-center">
         <UserSignUp />
+        <GoogleLogin
+        clientId="888291695738-c01715sl74g8g2plmenm40bgcccofvi0.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+        />
     </div>
   );
 }
