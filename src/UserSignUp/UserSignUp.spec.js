8ad7d3143354f4ab7ded40ae4a7ce730ecd53f54
lalaura.lourenco <laura.lourenco@thoughtworks.com>
2020-06-15 17:17:19 -0300

    Tests if pipeline will break

--- src/UserSignUp/UserSignUp.spec.js
+++ src/UserSignUp/UserSignUp.spec.js
@@ -1,39 +1,42 @@
-import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-import axiosMock from 'axios'
-import UserSignUp from './UserSignUp.js'
-import baseURL from '../base'
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import axiosMock from "axios";
+import UserSignUp from "./UserSignUp.js";
+import baseURL from "../base";
 
-jest.mock('axios')
+jest.mock("axios");
 
-test('loads and displays user sign up form', () => {
-    const form = render(<UserSignUp />)
-    const name = form.getByTestId('name')
-    const email = form.getByTestId('email')
-    const submit = form.getByTestId('submit')
+test("loads and displays user sign up form", () => {
+  const form = render(<UserSignUp />);
+  const name = form.getByTestId("name");
+  const email = form.getByTestId("email");
+  const submit = form.getByTestId("submit");
 
-    expect(name).toBeInTheDocument()
-    expect(email).toBeInTheDocument()
-    expect(submit).toBeInTheDocument()
-})
+  expect(name).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
+  expect(submit).toBeInTheDocument();
+});
 
+test("submits user name and email", async () => {
+  const mockedUserInfo = {
+    name: "Agatha Silva",
+    email: "agatha@example.com",
+  };
+  const mockedURL = `${baseURL}/user`;
 
-test('submits user name and email', async () => {
-    const mockedUserInfo = {
-        name: 'Agatha Silva',
-        email: 'agatha@example.com',
-    }
-    const mockedURL = `${baseURL}/user` 
+  const form = render(<UserSignUp />);
+  const name = form.getByTestId("name");
+  const email = form.getByTestId("email");
+  const submit = form.getByTestId("submit");
+  fireEvent.change(name, { target: { value: mockedUserInfo.name } });
+  fireEvent.change(email, { target: { value: mockedUserInfo.email } });
+  fireEvent.click(submit);
 
-    const form = render(<UserSignUp />)
-    const name = form.getByTestId('name')
-    const email = form.getByTestId('email')
-    const submit = form.getByTestId('submit')
-    fireEvent.change(name, { target: { value: mockedUserInfo.name } })
-    fireEvent.change(email, { target: { value: mockedUserInfo.email } })
-    fireEvent.click(submit)
-    
-    expect(axiosMock.post).toHaveBeenCalledTimes(1)
-    expect(axiosMock.post).toHaveBeenCalledWith(mockedURL, mockedUserInfo)
-  })
+  expect(axiosMock.post).toHaveBeenCalledTimes(1);
+  expect(axiosMock.post).toHaveBeenCalledWith(mockedURL, mockedUserInfo);
+});
+
+test("tests if pipeline will break", () => {
+  expect(12).toBe(21);
+});
