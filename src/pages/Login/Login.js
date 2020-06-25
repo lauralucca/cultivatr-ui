ef7f1dc3e48a0fd32e31c4c78db4940f8ba05536
Matheus Henrique Pereira Santos <matheus.santos@Matheuss-MacBook-Pro.local>
2020-06-25 18:26:41 -0300

    [Laura|Matheus] Changes folder organizing

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import GoogleLogin from 'react-google-login'
+import {User} from '../../api'
+
+const responseGoogleSuccess = async (response) => {
+    const userInfo = {
+        name: response.profileObj.name,
+        email: response.profileObj.email
+    }
+   
+    await User.saveNew(userInfo)
+  }
+
+const responseGoogleFailure = (response) => {
+    console.log('failure', response);
+  }
+
+
+
+const Login = () => (
+    <GoogleLogin
+    clientId="888291695738-c01715sl74g8g2plmenm40bgcccofvi0.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={responseGoogleSuccess}
+    onFailure={responseGoogleFailure}
+    cookiePolicy={'single_host_origin'}
+    />
+
+)
+
+export default Login
