8bf81766a7f17b25d2dfa1377bbb033caca5c012
lalaura.lourenco <laura.lourenco@thoughtworks.com>
2020-07-07 17:50:25 -0300

    [Laura|Matheus] Removes unnecessary comments on api.js

--- src/api.js
+++ src/api.js
@@ -16,19 +16,3 @@ export const User = {
 }
 
 export default HelloApi
-
-//// Optionally the request above could also be done as
-//axios.get('/user', {
-//    params: {
-//      ID: 12345
-//    }
-//  })
-//  .then(function (response) {
-//    console.log(response);
-//  })
-//  .catch(function (error) {
-//    console.log(error);
-//  })
-//  .then(function () {
-//    // always executed
-//  });
