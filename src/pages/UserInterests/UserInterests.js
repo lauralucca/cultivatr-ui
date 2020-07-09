c6e8361e3d72cb02b52fe31ae53dbceceaa088b4
Matheus Henrique Pereira Santos <matheus.santos@Matheuss-MacBook-Pro.local>
2020-07-09 18:42:43 -0300

    [Laura|Matheus] Shows interest list when user adds new interest

--- src/pages/UserInterests/UserInterests.js
+++ src/pages/UserInterests/UserInterests.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const handleSubmit = async (event) => {
   event.preventDefault();
   event.target.reset();
 };
 
+
+
 const UserInterests = ({ onSubmit = handleSubmit }) => {
+  const [interestList, setInterestList] = useState([])
+  const [newInterest, setNewInterest] = useState()
+
+  const addNewInterestToInterestList = () => {
+  
+    const newList = [newInterest]
+    interestList.map(i => newList.push(i))
+    setInterestList(newList)
+  }
+
+
   return (
     <form onSubmit={onSubmit}>
-      <input type="text" data-testid="teaching-interest" placeholder="Interesse" />
-      <input type="button" data-testid="add-interest" value="Adicionar"/>
+      <input type="text" data-testid="teaching-interest" placeholder="Interesse" onChange={e => setNewInterest(e.target.value)} />
+      <input
+        type="button"
+        data-testid="add-interest"
+        onClick={addNewInterestToInterestList}
+        value="Adicionar" />
+      <ul data-testid="interest-list">
+        {
+          interestList.map((interest, index) => <li key={index}>{interest}</li>)
+        }
+      </ul>
       <input type="submit" data-testid="submit" label="Salvar" />
     </form>
   );
--- src/pages/UserInterests/UserInterests.spec.js
+++ src/pages/UserInterests/UserInterests.spec.js
@@ -1,20 +1,34 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import UserInterests from "./UserInterests.js";
+import React from "react"
+import { render, fireEvent, within } from "@testing-library/react"
+import "@testing-library/jest-dom/extend-expect"
+import UserInterests from "./UserInterests.js"
 
-jest.mock("axios");
+jest.mock("axios")
 
 test("loads and displays user interests form", () => {
-  const { getByTestId } = render(<UserInterests />);
-  // const learningGoals = getByTestId("learningGoals");
-  const teachingGoals = getByTestId("teaching-interest");
-  const addButton = getByTestId("add-interest");
-  const saveButton = getByTestId("submit");
+  const { getByTestId } = render(<UserInterests />)
+  // const learningGoals = getByTestId("learningGoals")
+  const teachingInterest = getByTestId("teaching-interest")
+  const addButton = getByTestId("add-interest")
+  const saveButton = getByTestId("submit")
 
   // expect(learningGoals).toBeInTheDocument();
-  expect(teachingGoals).toBeInTheDocument();
-  expect(addButton).toBeInTheDocument();
-  expect(saveButton).toBeInTheDocument();
+  expect(teachingInterest).toBeInTheDocument()
+  expect(addButton).toBeInTheDocument()
+  expect(saveButton).toBeInTheDocument()
+});
+
+test("adds new interest to interests list", () =>{
+  const {getByTestId} = render(<UserInterests/>)
+  const addButton = getByTestId("add-interest")
+  const teachingInterest = getByTestId("teaching-interest")
+  const interestList = getByTestId("interest-list")
+  
+  fireEvent.change(teachingInterest, { target: { value: "TDD"} });
+  fireEvent.click(addButton)
+
+  expect(within(interestList).getByText("TDD")
+  ).toBeInTheDocument()
+  
 });
 
