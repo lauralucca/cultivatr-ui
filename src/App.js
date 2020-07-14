1895f112af6f8381b02b45e76d87c302ecb304b9
lalaura.lourenco <laura.lourenco@thoughtworks.com>
2020-07-14 17:24:56 -0300

    [Laura|Matheus] Fixes formatting warnings

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-import React from 'react';
-import { BrowserRouter as Router,Switch, Route } from "react-router-dom"
-import { ThemeProvider } from 'styled-components';
-import { MAIN_THEME } from './styles/theme'
-import Header from './components/Header/Header';
-import Login from './pages/Login/Login'
-import Dashboard from './pages/Dashboard/Dashboard';
-import UserSignUp from './pages/UserSignUp/UserSignUp'
-import UserInterests from './pages/UserInterests/UserInterests'
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
+import { MAIN_THEME } from "./styles/theme"
+import Header from "./components/Header/Header"
+import Login from "./pages/Login/Login"
+import Dashboard from "./pages/Dashboard/Dashboard"
+import UserSignUp from "./pages/UserSignUp/UserSignUp"
+import UserInterests from "./pages/UserInterests/UserInterests"
 
 function App() {
   return (
-    <ThemeProvider theme={ MAIN_THEME }>
+    <ThemeProvider theme={MAIN_THEME}>
       <Router>
         <Header />
-        <Switch>
-          <div className="text-center">
-              <Route exact path="/">
-                <Login />
-              </Route>
-              <Route path="/login">
-                <Login />
-              </Route>
-              <Route path="/inicio">
-                <Dashboard />
-              </Route>
-              <Route path="/interesses">
-                <UserInterests />
-              </Route>
-              <Route path="/cadastro">
-                <UserSignUp />
-              </Route>
-          </div>
-        </Switch>
+        <div className="text-center">
+          <Switch>
+            <Route exact path="/">
+              <Login />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/inicio">
+              <Dashboard />
+            </Route>
+            <Route path="/interesses">
+              <UserInterests />
+            </Route>
+            <Route path="/cadastro">
+              <UserSignUp />
+            </Route>
+          </Switch>
+        </div>
       </Router>
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
